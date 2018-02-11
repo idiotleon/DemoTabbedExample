b612c6022e04591eb11411a028fff840b3b14e15
Yang Lu <leon@yangs-macbook-pro.local>
2018-02-11 15:28:22 -0800

    Initial Commit

---
+++ DemoTabbedExample/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DemoTabbedExample
+//
+//  Created by Yang Lu on 2/11/18.
+//  Copyright © 2018 7. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
